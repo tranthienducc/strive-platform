4099b9b6e7a4112630a74fe3667784c589cee90e
kind012 <tranthienduc1001@gmal.com>
2024-08-17 16:52:27 +0700

    add new images and icons

---
+++ components/icons/IconsCheck.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const IconsCheck = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      fill="#a3a3a3"
+      className="size-6 "
+    >
+      <g fill="#a3a3a3" height="fill">
+        <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm45.66,85.66-56,56a8,8,0,0,1-11.32,0l-24-24a8,8,0,0,1,11.32-11.32L112,148.69l50.34-50.35a8,8,0,0,1,11.32,11.32Z"></path>
+      </g>
+    </svg>
+  );
+};
+
+export default IconsCheck;
---
+++ components/icons/IconsDate.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const IconsDate = () => {
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      className="size-4 fill-current opacity-90"
+    >
+      <path d="M13 2h-2V1h-1v1H6V1H5v1H3c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h10c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1Zm0 11H3V6h10v7Zm0-8H3V3h2v1h1V3h4v1h1V3h2v2Z"></path>
+    </svg>
+  );
+};
+
+export default IconsDate;

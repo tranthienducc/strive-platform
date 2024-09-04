ddb86075bcae053a34983b18aad7d4bcadf3e664
kind012 <tranthienduc1001@gmal.com>
2024-09-04 11:41:58 +0700

    remove Metadata

--- app/inspiration/inpiration-detail/[slug]/page.tsx
+++ app/inspiration/inpiration-detail/[slug]/page.tsx
@@ -1,31 +1,4 @@
-import { Metadata } from "next";
-import { getInspirations } from "@/state/functions";
 import DetailInspirationCard from "@/components/inspiration-shared/DetailInspirationCard";
-
-interface Props {
-  params: {
-    slug: string;
-  };
-}
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const slug = params.slug;
-
-  const filterInspirations = await getInspirations(slug);
-
-  return {
-    title: filterInspirations?.title,
-    description: filterInspirations?.description,
-    keywords: "Strive Platform",
-    openGraph: {
-      title: filterInspirations?.title,
-      description: filterInspirations?.description,
-      images: [
-        filterInspirations?.coverImage || "/inspiration-detail-page.png",
-      ],
-    },
-  };
-}
 
 const DetailInspiration = () => {
   return (

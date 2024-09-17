5fd6410877c3df5db30038a5848ab99f6f3aff12
kind012 <tranthienduc1001@gmal.com>
2024-09-17 17:13:11 +0700

    update midleware part 2

--- middleware.ts
+++ middleware.ts
@@ -6,39 +6,31 @@ import { getAuth } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher(["/cms(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  // Check if the route is protected and enforce authentication if it is
   if (isProtectedRoute(req)) auth().protect();
+
   const url = req.nextUrl;
-  let hostname = req.headers
-    .get("host")!
-    .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_BASE_DOMAIN}`);
+  const hostname = req.headers.get("host")!;
+  const path = `${url.pathname}${url.search}`;
 
-  // Nếu hostname có "www." hoặc các subdomain không cần thiết
-  hostname = hostname.replace(/^www\./, "");
-
-  const searchParams = req.nextUrl.searchParams.toString();
-  const path = `${url.pathname}${searchParams ? `?${searchParams}` : ""}`;
-
-  // Xử lý rewrite cho domain chính
-  if (hostname === process.env.NEXT_PUBLIC_BASE_DOMAIN) {
-    const session = getAuth(req);
-    if (!session && path !== "/sign-in") {
-      return NextResponse.redirect(new URL("/sign-in", req.url));
-    } else if (session && path === "/sign-in") {
-      return NextResponse.redirect(new URL("/", req.url));
+  // Xử lý cho domain chính
+  if (
+    hostname === process.env.NEXT_PUBLIC_BASE_DOMAIN ||
+    hostname === "localhost:3000"
+  ) {
+    // Chỉ kiểm tra session cho các route cần bảo vệ
+    if (isProtectedRoute(req)) {
+      const session = getAuth(req);
+      if (!session) {
+        return NextResponse.redirect(new URL("/sign-in", req.url));
+      }
     }
     return NextResponse.rewrite(new URL(path, req.url));
   }
 
-  // Rewrite cho localhost
-  if (hostname === "localhost:3000") {
-    return NextResponse.rewrite(new URL(path, req.url));
-  }
-
-  // Rewrite mọi thứ còn lại về dynamic route `/[domain]/[slug]`
+  // Xử lý cho các subdomain
   return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/dashboard", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };

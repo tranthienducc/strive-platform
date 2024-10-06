cb7ba188ea6c79329b3c71af1ff34ecd4ac23fb7
kind012 <tranthienduc1001@gmal.com>
2024-10-06 18:34:01 +0700

    fix issue

--- app/cms/ai/page.tsx
+++ app/cms/ai/page.tsx
@@ -1,14 +1,15 @@
-import Chat from "@/app/cms/ai/_component/Chat";
+// import Chat from "@/app/cms/ai/_component/Chat";
 
-const initialMessages = ["this is initial message chat bot ai"];
+// const initialMessages = ["this is initial message chat bot ai"];
 
 export default async function Ai() {
   return (
-    <Chat
-      message={initialMessages.map((m: any, i: any) => ({
-        ...m,
-        id: i.toString(),
-      }))}
-    />
+    // <Chat
+    //   message={initialMessages.map((m: any, i: any) => ({
+    //     ...m,
+    //     id: i.toString(),
+    //   }))}
+    // />
+    <div></div>
   );
 }

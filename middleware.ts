943457f13cd64bc8976a43aa8610e1567bee13d7
kind012 <tranthienduc1001@gmal.com>
2024-10-07 10:50:00 +0700

    fix bug middleware T_T

--- middleware.ts
+++ middleware.ts
@@ -26,5 +26,5 @@ export default clerkMiddleware(async (auth, req) => {
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/cms", "/(api|trpc)(.*)"],
 };

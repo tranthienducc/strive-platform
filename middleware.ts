6523c9237a6f328e54188609dde2a12d53402e25
kind012 <tranthienduc1001@gmal.com>
2024-10-27 17:58:00 +0700

    fix midd

--- middleware.ts
+++ middleware.ts
@@ -6,21 +6,25 @@ const isProtectedRoute = createRouteMatcher(["/cms(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
-
   const url = req.nextUrl;
   const hostname = req.headers.get("host")!;
-  const path = `${url.pathname}${url.search}`;
+  const isRootPath = url.pathname === "/";
 
   // Xử lý cho domain chính
   if (
     hostname === process.env.NEXT_PUBLIC_BASE_DOMAIN ||
     hostname === "localhost:3000"
   ) {
-    // Chỉ kiểm tra session cho các route cần bảo vệ
-    return NextResponse.rewrite(new URL(path, req.url));
+    if (isRootPath) {
+      return NextResponse.rewrite(new URL("/", req.url));
+    }
+    return NextResponse.rewrite(
+      new URL(`${url.pathname}${url.search}`, req.url)
+    );
   }
 
   // Xử lý cho các subdomain
+  const path = isRootPath ? "/" : `${url.pathname}${url.search}`;
   return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
 });
 
